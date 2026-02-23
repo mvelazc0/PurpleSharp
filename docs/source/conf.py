871aec320ce03c004eb69153c45baf7532ca7e15
Mauricio Velazco <mvelazco@microsoft.com>
2026-02-22 21:45:19 -0500

    readthedocs 3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,10 +52,6 @@ source_suffix = {
 #
 html_theme = 'sphinx_rtd_theme'
 
-html_theme_options = {
-    'analytics_id': 'G-LEGFEVGH4F',
-}
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
